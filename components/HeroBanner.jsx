06cf6d601eed69b85c60ee3a1130cb981ad4ca43
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-09-28 15:37:26 +0500

    update herobanner to back whhere it was

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,19 +1,18 @@
-
+import config from '../lib/config';
+const heroimages = config.heroItems.map(item => item.label);
 import React from 'react'
 import Image from 'next/image'
 import {CgShoppingCart} from 'react-icons/cg'
-import config from '../lib/config';
+import headerImg from '../src/assets/header.png'
+import featured1 from '../src/assets/featured/Featured1.png';
+import featured2 from '../src/assets/featured/Featured2.png';
+import featured3 from '../src/assets/featured/Featured3.png';
+import featured4 from '../src/assets/featured/Featured4.png';
 import Link from 'next/link';
 
 
-const heroimages = config.heroItems.map(item => item.label);
 const heroLabels = config.heroItems.map(item => item.label);
 
-const headerImg = heroimages[0].imagePath;
-const featured1 = heroimages[1].imagePath;
-const featured2 = heroimages[2].imagePath;
-const featured3 = heroimages[3].imagePath;
-const featured4 = heroimages[4].imagePath;
 
 const HeroBanner = () => {
   return (
@@ -29,16 +28,16 @@ const HeroBanner = () => {
             </div>
 
             <div className='header-featured'>
-                <Image src={heroimages[1].imagePath} width={100} height={35} alt='img' />
-                <Image src={heroimages[2].imagePath} width={100} height={35} alt='img' />
-                <Image src={heroimages[3].imagePath} width={100} height={35} alt='img' />
-                <Image src={heroimages[4].imagePath} width={100} height={35} alt='img' />
+                <Image src={featured1} width={100} height={35} alt='img' />
+                <Image src={featured2} width={100} height={35} alt='img' />
+                <Image src={featured3} width={100} height={35} alt='img' />
+                <Image src={featured4} width={100} height={35} alt='img' />
             </div>
         </div>
 
         <div className='header-right-side'>
             <div className='header-circle'>
-                <Image className='header-img' src={heroimages[0].imagePath} width={650} height={650} alt='header image' />
+                <Image className='header-img' src={headerImg} width={650} height={650} alt='header image' />
             </div>
         </div>
     </header>
