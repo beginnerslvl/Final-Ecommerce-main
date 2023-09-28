8b86c5445c5860bb0b1fe541691cd9c285b599d4
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-09-28 16:39:50 +0500

    update FeaturesBanner

--- components/FeaturesBanner.jsx
+++ components/FeaturesBanner.jsx
@@ -2,41 +2,65 @@ import React from 'react'
 import Image from 'next/image'
 import img from '../src/assets/feature.png'
 import Link from 'next/link'
+import config from '../lib/config';
+const fearturestext = config.fearturestext.map(item => item.label);
+
+
+const openning_line = fearturestext[0]
+const background_text = fearturestext[1]
+
+
+
+const point1 = fearturestext[2]
+const point1B = fearturestext[3]
+
+
+
+const point2 = fearturestext[4]
+const point2_b = fearturestext[5]
+
+const point3 = fearturestext[6]
+const point3_b = fearturestext[7]
+
+const point4 = fearturestext[8]
+const point4_b = fearturestext[9]
+
+const small_description = fearturestext[10]
 
 const FeaturesBanner = () => {
   return (
     <section className='features-section'>
       <div className='title'>
-        <h1>Unique and Authentic Vintage Designer Jewellery</h1>
+        <h1>{openning_line}</h1>
       </div>
 
       <div className='content'>
         <div className='left'>
           <div className="feature-background">
-            Different from others
+            {background_text}
           </div>
           <div>
-            <h3>Using Good Quality Materials</h3>
-            <p>Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+            <h3>{point1}</h3>
+            <p>{point1B}</p>
           </div>
           <div>
-            <h3>100% Handmade Products</h3>
-            <p>Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+            <h3>{point2}</h3>
+            <p>{point2_b}</p>
           </div>
           <div>
-            <h3>Modern Fashion Design</h3>
-            <p>Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+            <h3>{point3}</h3>
+            <p>{point3_b}</p>
           </div>
           <div>
-            <h3>Discount for Bulk Orders</h3>
-            <p>Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
+            <h3>{point4}</h3>
+            <p>{point4_b}</p>
           </div>
         </div>
 
         <div className='right'>
           <Image src={img} width={300} height={350} alt='img' />
           <div>
-            <p>This piece is ethically crafted in our small family-owned workshop in Peru with unmatched attention to detail and care. The Natural color is the actual natural color of the fiber, undyed and 100% traceable.</p>
+            <p>{small_description}</p>
             <Link href={'/products'}>
               <button className='btn' type='button'>See All Product</button>
             </Link>
