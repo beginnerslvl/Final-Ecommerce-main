ffb08a086bed8c87a65233be319d114b2ce0dcf7
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-09-29 01:01:01 +0500

    update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Toaster } from 'react-hot-toast'
-import '../styles/globals.css'
-import { Layout } from '../components'
-import { StateContext } from '../context/StateContext'
+// In your App.js or App.jsx
+import React, { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import '../styles/globals.css';
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+import config from '../lib/config';
+const design = config.design.map(item => item.label);
+const color1 = design[0]
+const color2 = design[1]
+const color3 = design[2]
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Access the root element to set CSS variables
+    const root = document.documentElement;
+
+    // Update CSS variables with imported values
+    root.style.setProperty('--primaryColor', color1);
+    root.style.setProperty('--primaryColor1', color2);
+    root.style.setProperty('--primaryColor2', color3);
+  }, []);
+
   return (
     <StateContext>
       <Layout>
@@ -12,5 +28,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </StateContext>
-  )
+  );
 }
