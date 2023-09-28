0d466f0feed572e650ccdaf358228f7787c55904
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-09-28 16:56:05 +0500

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../src/assets/logo/Logo.png'
 import {GrFacebookOption, GrTwitter, GrLinkedinOption} from 'react-icons/gr'
+import config from '../lib/config';
+const footertext = config.footertext.map(item => item.label);
 
+const slogon = footertext[0]
+
+const copywriting = footertext[1]
+// const subheading1 = footertext[1]
+// const subheading2 = footertext[1]
+// const subheading3 = footertext[1]
+// const subheading4 = footertext[1]
+// const subheading5 = footertext[1]
 const Footer = () => {
   return (
     <footer>
       <div className='footer'>
         <div className='logo'>
           <Image src={logo} width={180} height={30} alt='logo' />
-          <p>Small, artisan label that offers a thoughtfully curated collection of high quality everyday essentials made.</p>
+          <p>{slogon}</p>
           <div className='icon-container'>
             <div><GrTwitter size={20} /></div>
             <div><GrFacebookOption size={20} /></div>
@@ -47,9 +57,7 @@ const Footer = () => {
       </div>
 
       <div className='copyright'>
-        <p>Copyright © 2022 Dine Market</p>
-        <p>Design by. <span>Weird Design Studio</span></p>
-        <p>Code by. <span>shabrina12 on github</span></p>
+        <p>{copywriting}</p>
       </div>
     </footer>
   )
