0fc10b57238bb3f1208746dc7a41d2a213391b28
Waleed Ajmal <127334056+Majboor@users.noreply.github.com>
2023-09-28 14:53:09 +0500

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -60,17 +60,16 @@ const Navbar = ({Searchproducts}) => {
             <RiCloseLine  color='black' fontSize={27} className='close_icon' onClick={() => setToggleMenu(false)} />
             <ul className='navbar-smallscreen_links'>
               <Link href='/cart'>
-                <button className='cart-small-screen' onClick={() => setShowCart(false)}>   
-                  <CgShoppingCart size={22} />
-                  <span className='cart-item-qty'>{totalQty}</span> 
-                </button>
-              </Link>
+                  <button className='cart-small-screen' onClick={() => setShowCart(false)}>   
+                    <CgShoppingCart size={22} />
+                    <span className='cart-item-qty'>{totalQty}</span> 
+                  </button>
+              </Link> 
               {config.smallScreenNavItems.map((item, index) => (
                 <Link key={index} href={item.href}>
                   <li>{item.label}</li>
                 </Link>
               ))}
-              {/* You can add more small screen navigation items to the config file */}
             </ul>
           </div>
         )}
